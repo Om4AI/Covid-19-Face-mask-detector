033c3720098872867ab9d7e05997997f5d13e504
Om Mule <dgr8om@gmail.com>
2021-03-29 00:01:17 +0530

    Bounding boxes and Face detection

---
+++ Face detection.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+cascPath = os.path.dirname(
+    cv2.__file__) + "//data//haarcascade_frontalface_alt2.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+video_capture = cv2.VideoCapture(0)
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray,
+                                         scaleFactor=1.1,
+                                         minNeighbors=5,
+                                         minSize=(60, 60),
+                                         flags=cv2.CASCADE_SCALE_IMAGE)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h),(0,255,0), 2)
+        # Display the resulting frame
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
